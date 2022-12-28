d6f732dc32594a556792ecc693503d40ec4acbdc
Kenny <Kennyytran95@gmail.com>
2022-12-28 02:31:06 -0800

    dp fin

--- dynamic-programming/allConstruct.js
+++ dynamic-programming/allConstruct.js
@@ -16,18 +16,72 @@ function allConstruct(target, wordBank, memo = {}) {
   return memo[target];
 }
 
-console.log(allConstruct("apple", ["ap", "a", "pple", "ppl", "ple"]));
-console.log(allConstruct("orange", ["ap", "pple", "ppl", "ple"]));
+// console.log(allConstruct("apple", ["ap", "a", "pple", "ppl", "ple"]));
+// console.log(allConstruct("orange", ["ap", "pple", "ppl", "ple"]));
+// console.log(
+//   allConstruct("scimitar", ["s", "cim", "it", "ar", "scimiw", "shimmy", "shim"])
+// );
+// console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+// console.log(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
+// console.log(
+//   allConstruct("skatboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+// );
+// console.log(
+//   allConstruct("eeeeeeeeeeeeeef", [
+//     "e",
+//     "ee",
+//     "eee",
+//     "eeeee",
+//     "eeeeeeee",
+//     "eeeeeeee",
+//     "eeeeeeeee",
+//   ])
+// );
+
+/* Tabulation */
+
+function T_allConstruct(target, wordBank) {
+  const table = Array(target.length + 1)
+    .fill()
+    .map(() => []);
+
+  table[0] = [[]];
+
+  for (let i = 0; i <= target.length; i++) {
+    for (let word of wordBank) {
+      if (target.slice(i, i + word.length) === word) {
+        let newCombo = table[i].map((prefixCombo) => {
+          return [...prefixCombo, word];
+        });
+        table[i + word.length] = [...table[i + word.length], ...newCombo];
+        // table[i + word.length].push(...newCombo);  <-- also works
+      }
+    }
+  }
+  return table[target.length];
+}
+
+console.log(T_allConstruct("apple", ["ap", "a", "pple", "ppl", "ple"]));
+console.log(T_allConstruct("orange", ["ap", "pple", "ppl", "ple"]));
 console.log(
-  allConstruct("scimitar", ["s", "cim", "it", "ar", "scimiw", "shimmy", "shim"])
+  T_allConstruct("scimitar", [
+    "s",
+    "cim",
+    "it",
+    "ar",
+    "scimiw",
+    "shimmy",
+    "shim",
+    "scimitar",
+  ])
 );
-console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
-console.log(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
+console.log(T_allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(T_allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
 console.log(
-  allConstruct("skatboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+  T_allConstruct("skatboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
 );
 console.log(
-  allConstruct("eeeeeeeeeeeeeef", [
+  T_allConstruct("eeeeeeeeeeeeeef", [
     "e",
     "ee",
     "eee",
