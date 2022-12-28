254f2e343b7602b3b35dc4f2328c074bfa352bfd
Kenny <Kennyytran95@gmail.com>
2022-12-27 16:03:40 -0800

    count con

--- dynamic-programming/canConstruct.js
+++ dynamic-programming/canConstruct.js
@@ -59,3 +59,44 @@ console.log(
     "eeeeeeeee",
   ])
 );
+
+/* Tabulation */
+
+function T_canConstruct(target, wordBank) {
+  const table = Array(target.length + 1).fill(false);
+  table[0] = true;
+  for (let i = 0; i < target.length; i++) {
+    if (table[i] === true) {
+      for (let word of wordBank) {
+        if (target.slice(i, i + word.length) === word) {
+          table[i + word.length] = true;
+        }
+      }
+    }
+  }
+  return table[target.length];
+}
+
+console.log(T_canConstruct("apple", ["ap", "pple", "ppl", "ple"]));
+console.log(T_canConstruct("orange", ["ap", "pple", "ppl", "ple"]));
+console.log(
+  T_canConstruct("scimitar", [
+    "s",
+    "cim",
+    "it",
+    "ar",
+    "scimiw",
+    "shimmy",
+    "shim",
+  ])
+);
+console.log(T_canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(
+  T_canConstruct("skatboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+);
+console.log(
+  T_canConstruct(
+    "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+    ["e", "ee", "eee", "eeeee", "eeeeeeee", "eeeeeeee", "eeeeeeeee"]
+  )
+);
--- dynamic-programming/countConstruct.js
+++ dynamic-programming/countConstruct.js
@@ -44,3 +44,47 @@ console.log(
     "eeeeeeeee",
   ])
 );
+
+function T_countConstruct(target, wordBank) {
+  const table = Array(target.length + 1).fill(0);
+  table[0] = 1;
+
+  for (let i = 0; i <= target.length; i++) {
+    for (let word of wordBank) {
+      if (target.slice(i, i + word.length) === word) {
+        table[i + word.length] += table[i];
+      }
+    }
+  }
+  return table[target.length];
+}
+
+console.log(T_countConstruct("apple", ["ap", "a", "pple", "ppl", "ple"]));
+console.log(T_countConstruct("orange", ["ap", "pple", "ppl", "ple"]));
+console.log(
+  T_countConstruct("scimitar", [
+    "s",
+    "cim",
+    "it",
+    "ar",
+    "scimiw",
+    "shimmy",
+    "shim",
+  ])
+);
+console.log(T_countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(T_countConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
+console.log(
+  T_countConstruct("skatboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+);
+console.log(
+  T_countConstruct("eeeeeeeeeeeeeeeeeeef", [
+    "e",
+    "ee",
+    "eee",
+    "eeeee",
+    "eeeeeeee",
+    "eeeeeeee",
+    "eeeeeeeee",
+  ])
+);
